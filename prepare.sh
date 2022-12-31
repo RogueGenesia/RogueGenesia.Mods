5b2a5212b0bfe200bda8771968c719b06f1fe711
Jason Couture <jasonc@alertr.info>
2022-12-30 20:28:35 -0500

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -15,8 +15,7 @@ wget -nc https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sourc
 mv winehq-jammy.sources /etc/apt/sources.list.d/
 apt-get update -y
 apt-get install -y --no-install-recommends ca-certificates gnupg cabextract unzip
-apt-get install -y --install-recommends xvfb lib32gcc-s1 steamcmd
-apt-get install -y --install-recommends winehq-staging=7.22~jammy-1
+apt-get install -y --install-recommends xvfb lib32gcc-s1 steamcmd winehq-staging=7.22~jammy-1
 
 ln -s /usr/games/steamcmd /usr/bin/steamcmd
 mkdir -p /workspace/work/game
